2cd5a3f77fa03c16dc98a933d2d60883c7bca1dc
Matt Guttman <msguttman@gmail.com>
2020-04-28 04:13:30 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -17,12 +17,13 @@ def nyc_pigeon_organizer(data)
           #for each terminal name, get the info for this chain.
           pige_name = data[data.keys[count]].values[count1][val_count].to_s
           new_pige_atr = data.keys[count].to_sym
-          new_pige_value = data[data.keys[count]].keys[count1].to_s.split
+          new_pige_value = data[data.keys[count]].keys[count1].to_s
 
           #if the name has already been added, add the new data, otherwise add the new data to this name
           if new_pigeon_data.include? pige_name
 
             if new_pigeon_data[pige_name].include? new_pige_atr
+              
               new_values = new_pigeon_data[pige_name].values
               new_values << new_pige_value
               new_pigeon_data[pige_name][new_pige_atr] = new_values.flatten
@@ -41,9 +42,10 @@ def nyc_pigeon_organizer(data)
 
         count1 += 1
       end
-      count += 1
-
+      
+    count += 1
   end
+  
   new_pigeon_data
   
 end
