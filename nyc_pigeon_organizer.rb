fc7f98cf91a5912a6c22092516055c4d8264f48f
Matt Guttman <msguttman@gmail.com>
2020-04-28 04:05:57 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,10 +1,10 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
   
-  #this will store the new hash
+  #this will store the new reversed hash
   new_pigeon_data = Hash.new
 
-  #start iterating over the hash
+  #start iterating over the current hash
   count = 0
   while count < data.keys.length
 
@@ -16,31 +16,34 @@ def nyc_pigeon_organizer(data)
           
           #for each terminal name, get the info for this chain.
           pige_name = data[data.keys[count]].values[count1][val_count].to_s
-          new_pige_atr = data.keys[count].to_sym 
-          new_pige_value = data[data.keys[count]].keys[count1].to_s.split
-          
+          new_pige_atr = data.keys[count].to_sym
+          new_pige_value = data[data.keys[count]].keys[count1].to_s
+
           #if the name has already been added, add the new data, otherwise add the new data to this name
           if new_pigeon_data.include? pige_name
-            puts "okay"
+
             if new_pigeon_data[pige_name].include? new_pige_atr
-              new_pigeon_data[pige_name][new_pige_atr].append(new_pige_value)
+              new_values = new_pigeon_data[pige_name].values
+              new_values << new_pige_value
+              new_pigeon_data[pige_name][new_pige_atr] = new_values
             else
                 new_pigeon_data[pige_name][new_pige_atr] = new_pige_value
-            end
+           end
+
           else
             new_pigeon_data[pige_name] = {
               new_pige_atr => new_pige_value
             }
           end
+
           val_count += 1
         end
 
         count1 += 1
-        
       end
-    count += 1
-  end
+      count += 1
 
- new_pigeon_data
+  end
+  new_pigeon_data
   
 end
